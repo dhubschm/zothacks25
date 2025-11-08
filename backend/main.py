c41078d0a31f573dcfa10f0f061925408f4c665c
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-08 12:14:44 -0800

    Add user and event creation endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,41 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name:str = None
-    destination: str
+class User(BaseModel):
+    Email: str = None
+    Username: str = None
+    #is_done: bool = False
 
-items = []
-#changed something
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class Event(BaseModel):
+    CarType: str = None
+    Location: str = None
+    Capacity: int = None
 
-@app.get("/items/{idem_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
 
-@app.put("/items/{item_id}")
-def save_item(item_id: int, item: Item):
-    return {"item_name": item.destination, "item_id": item_id}
+users = []
+events = []
+
+@app.post("/users")
+def create_user(new_user: User):
+    users.append(new_user)
+    return new_user
+
+@app.post("/users/events")
+def create_event(new_event: Event):
+    events.append(new_event)
+    return events
+
+
+@app.get("/users/events", response_model=list[Event])
+def list_events(limit: int = 10):
+    return events[0:limit]
+
+
+@app.get("/users/{event_id}", response_model=Event)
+def get_event(event_id: int) -> Event:
+    if event_id < len(events):
+        return events[event_id]
+    else:
+        raise HTTPException(status_code=404, detail=f"Item {event_id} not found")
