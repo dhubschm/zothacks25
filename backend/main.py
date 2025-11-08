89391218da9974c668c4404cb74d8371d0f14022
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-08 15:25:33 -0800

    Fix errors in endpoints for joining events and viewing attendees

--- backend/main.py
+++ backend/main.py
@@ -17,38 +17,40 @@ class Event(BaseModel):
     Time: str = None
     Location: str = None
     Capacity: int = None
-    Attendees: List[str] = []
+    Attendees: List[User] = []
 
 users = []
-events = []
+events_list = []
 
-@app.post("/users")
+@app.post("/users", response_model=User)
 def create_user(new_user: User):
     users.append(new_user)
     return new_user
 
-@app.post("/users/events")
+@app.post("/users/events", response_model=Event)
 def create_event(new_event: Event):
-    events.append(new_event)
-    return events
+    events_list.append(new_event)
+    return new_event
 
-@app.post("/users/events/", response_model=Event)
-def join_event(event: Event, attendee: User):
+@app.post("/users/events/{event_id}", response_model=Event)
+def join_event(event_id: int, attendee: User):
+    event = events_list[event_id]
     event.Attendees.append(attendee)
-    return event.Attendees
+    return event
 
-@app.get("/users/events/attendees", response_model=Event)
-def list_attendees(event: Event, limit: int = 10):
+@app.get("/users/events/attendees", response_model=list[User])
+def list_attendees(event_id: int, limit: int = 10):
+    event = events_list[event_id]
     return event.Attendees[0:limit]
 
 @app.get("/users/events", response_model=list[Event])
 def list_events(limit: int = 10):
-    return events[0:limit]
-
+    return events_list[0:limit]
 
 @app.get("/users/{event_id}", response_model=Event)
 def get_event(event_id: int) -> Event:
-    if event_id < len(events):
-        return events[event_id]
+    if event_id < len(events_list):
+        event = events_list[event_id]
+        return event
     else:
         raise HTTPException(status_code=404, detail=f"Item {event_id} not found")
