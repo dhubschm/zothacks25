05ee6ec2bb9bf73140380fb51398cbba8e9f91da
Daniel Hurtarte <dhurtart@uci.edu>
2025-11-08 16:27:17 -0800

    Create size handling and adjust endpoint strings

--- backend/main.py
+++ backend/main.py
@@ -9,54 +9,49 @@ class User(BaseModel):
     Username: str = None
 
 class Driver(BaseModel):
-    UserInfo: User = None #Ingerits User information (Email and Username)
+    UserInfo: User = None #Inherits User information (Email and Username)
     Cartype: str = None
 
 class Event(BaseModel):
+    EventName: str = None
     Host: Driver = None #Inherits Driver information (Userinfo and Cartype)
     Time: str = None
     Location: str = None
-    Capacity: int = None
+    Capacity: int = None #change to available seating and decrement it? 
     Attendees: List[User] = []
 
 users = []
 events_list = []
 
 #Creates a User class and adds it to a list of users
-@app.post("/users", response_model=User)
+@app.post("/users/create/", response_model=User)
 def create_user(new_user: User):
     users.append(new_user)
     return new_user
 
 #Creates an Event class and adds it to a list of events
-@app.post("/users/events", response_model=Event)
+@app.post("/event/postevent/", response_model=Event)
 def create_event(new_event: Event):
     events_list.append(new_event)
     return new_event
 
 #Adds a User class to a list of attendees in an Event class
-@app.post("/users/events/{event_id}", response_model=Event)
-def join_event(event_id: int, attendee: User):
+@app.post("/events/join/{event_id}", response_model=Event)
+def join_event(event_id: int, attendee: User):  #implement size handling? 
     event = events_list[event_id]
-    event.Attendees.append(attendee)
-    return event
 
-#Displays the attendees (User classes) that exist in an Event class
-@app.get("/users/events/attendees", response_model=list[User])
-def list_attendees(event_id: int, limit: int = 10):
-    event = events_list[event_id]
-    return event.Attendees[0:limit]
-
-#Lists up to 10 events in an event list
-@app.get("/users/events", response_model=list[Event])
-def list_events(limit: int = 10):
-    return events_list[0:limit]
-
+    if event.Capacity > 0:
+        event.Attendees.append(attendee)
+        event.Capacity -= 1
+        return event
+    else:
+        raise HTTPException(status_code=503, detail=f"Event capacity for {event_id} is full")
+    
 #Retrieves a particular event in a list of events
-@app.get("/users/{event_id}", response_model=Event)
+@app.get("/events/viewevent/event_id}", response_model=Event)
 def get_event(event_id: int) -> Event:
     if event_id < len(events_list):
         event = events_list[event_id]
         return event
     else:
-        raise HTTPException(status_code=404, detail=f"Item {event_id} not found")
+        raise HTTPException(status_code=404, detail=f"Event {event_id} not found")
